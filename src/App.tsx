3f556d1f6ab65b58a3888ce5bbf7b67c633aba71
airtoxin <airtoxin@icloud.com>
2019-07-08 01:13:08 +0900

    Use emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { css } from "emotion";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={css({
+      backgroundColor: "blanchedalmond",
+      textAlign: "center",
+      color: "darkblue",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    })}>
+      <h1>Pow2</h1>
     </div>
   );
 };
